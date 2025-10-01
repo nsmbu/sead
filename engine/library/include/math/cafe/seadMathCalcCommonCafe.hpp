006ed7c29fe641e182c0e4f23fbeb770770dca91
Luminyx <79218624+Luminyx1@users.noreply.github.com>
2025-10-01 18:52:47 -0400

    Update seadMathCalcCommonCafe.hpp

--- engine/library/include/math/cafe/seadMathCalcCommonCafe.hpp
+++ engine/library/include/math/cafe/seadMathCalcCommonCafe.hpp
@@ -5,18 +5,18 @@
 namespace sead {
 
 template <>
-inline f32 MathCafe<f32>::rsqrt(f32 register x)
+inline f32 MathCafe<f32>::rsqrt(f32 x)
 {
     //SEAD_ASSERT_MSG(x >= 0.0f, "x[%f] is out of the domain.", x);
 
     if (x <= 0.0f)
         return 0.0f;
 
-    f32 register v0;
-    f32 register v1;
-    f32 register v2;
-    const f32 register HALF  = 0.5f;
-    const f32 register THREE = 3.0f;
+    f32 v0;
+    f32 v1;
+    f32 v2;
+    const f32 HALF  = 0.5f;
+    const f32 THREE = 3.0f;
 
 #ifdef SEAD_MATH_CAFE_RSQRT_USE_INTRINSICS
     // The use of inline assembly in an inline function can be very dangerous,
