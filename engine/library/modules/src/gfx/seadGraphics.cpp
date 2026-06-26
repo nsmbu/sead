ece48504b522f596844dec3cefd6cefbed9326f2
AboodXD <aboood40091@gmail.com>
2026-06-26 16:24:43 +0400

    engine/gfx/Graphics: Add initialize

--- engine/library/modules/src/gfx/seadGraphics.cpp
+++ engine/library/modules/src/gfx/seadGraphics.cpp
@@ -21,6 +21,18 @@ Graphics::~Graphics()
 {
 }
 
+void Graphics::initialize()
+{
+    lockDrawContext();
+    {
+        initializeImpl();
+
+        sead::GraphicsContext context;
+        context.apply();
+    }
+    unlockDrawContext();
+}
+
 void Graphics::lockDrawContext()
 {
     sead::Thread* current = ThreadMgr::instance()->getCurrentThread();
