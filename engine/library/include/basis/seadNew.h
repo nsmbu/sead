b0f30e4924c89b3a26219a97caa5f678c5852966
AboodXD <aboood40091@gmail.com>
2026-07-11 13:29:50 +0400

    engine/basis/New: Add operator new[] overload

--- engine/library/include/basis/seadNew.h
+++ engine/library/include/basis/seadNew.h
@@ -17,6 +17,7 @@ class Heap;
 
 void* operator new(size_t size);
 void* operator new[](size_t size);
+void* operator new[](size_t size, s32 alignment);
 
 void operator delete(void* ptr);
 void operator delete[](void* ptr);
