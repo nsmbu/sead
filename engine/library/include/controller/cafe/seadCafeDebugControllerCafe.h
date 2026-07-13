6d788a53d0ba637354a14b6a8c631a156f63fdb4
AboodXD <aboood40091@gmail.com>
2026-07-13 07:13:33 +0400

    engine/controller/cafe/CafeDebugPadDevice: Remove unused dtor

--- engine/library/include/controller/cafe/seadCafeDebugControllerCafe.h
+++ engine/library/include/controller/cafe/seadCafeDebugControllerCafe.h
@@ -11,7 +11,7 @@ class CafeDebugController : public Controller
 
 public:
     CafeDebugController(ControllerMgr* mgr, s32 channel);
-    virtual ~CafeDebugController() { }
+    ~CafeDebugController() override { }
 
     s32 getChannel() const { return mChannel; }
 
--- engine/library/include/controller/cafe/seadCafeDebugPadDeviceCafe.h
+++ engine/library/include/controller/cafe/seadCafeDebugPadDeviceCafe.h
@@ -14,7 +14,6 @@ class CafeDebugPadDevice : public ControlDevice
 
 public:
     explicit CafeDebugPadDevice(ControllerMgr* mgr);
-    virtual ~CafeDebugPadDevice() { }
 
     void calc() override;
 
