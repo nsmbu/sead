5719ef9759ade92760d00e5520e3e21ef46279d7
AboodXD <aboood40091@gmail.com>
2026-07-13 07:05:47 +0400

    engine/controller/cafe: Add CafeDebugController

---
+++ engine/library/include/controller/cafe/seadCafeDebugControllerCafe.h
@@ -0,0 +1,37 @@
+#ifndef SEAD_CAFE_DEBUG_CONTROLLER_CAFE_H_
+#define SEAD_CAFE_DEBUG_CONTROLLER_CAFE_H_
+
+#include <controller/seadController.h>
+
+namespace sead {
+
+class CafeDebugController : public Controller
+{
+    SEAD_RTTI_OVERRIDE(CafeDebugController, Controller)
+
+public:
+    CafeDebugController(ControllerMgr* mgr, s32 channel);
+    virtual ~CafeDebugController() { }
+
+    s32 getChannel() const { return mChannel; }
+
+    bool isConnected() const override { return mIsConnected; }
+
+private:
+#if 0 // defined(SEAD_TARGET_DEBUG)
+    void calcImpl_() override;
+#else
+    void calcImpl_() override { }
+#endif
+
+    void clampStickValue_(Vector2f* out, s8 x, s8 y);
+
+private:
+    s32 mChannel;
+    bool mIsConnected;
+};
+static_assert(sizeof(CafeDebugController) == 0x164, "sead::CafeDebugController size mismatch");
+
+} // namespace sead
+
+#endif // SEAD_CAFE_DEBUG_CONTROLLER_CAFE_H_
