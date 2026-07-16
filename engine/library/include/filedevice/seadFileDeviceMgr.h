88d572a64ea3421225ce2320f91041f15ddba08f
AboodXD <aboood40091@gmail.com>
2026-07-16 18:23:36 +0400

    engine/filedevice/FileDeviceMgr: Add mount paths getters (Cafe)

--- engine/library/include/filedevice/seadFileDeviceMgr.h
+++ engine/library/include/filedevice/seadFileDeviceMgr.h
@@ -102,8 +102,12 @@ public:
     RawErrorCode mountSDCard();
     RawErrorCode unmountSDCard();
 
+    const char* getSDCardMountPath() const { return mFSSDMountPath; }
+
     bool mountHostFileIO();
     bool unmountHostFileIO();
+
+    const char* getHostFileIOMountPath() const { return mFSHFIOMountPath; }
 
 protected:
     FSClient mFSClient;
