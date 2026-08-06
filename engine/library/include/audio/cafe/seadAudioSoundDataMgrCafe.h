2b1adf7319654723a6183153d2b0161405753908
STUPID Modder <stupidmoddersoup@gmail.com>
2026-08-06 11:16:51 -0300

    audio: Add AudioSoundDataMgrCafe

---
+++ engine/library/include/audio/cafe/seadAudioSoundDataMgrCafe.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <prim/seadSafeString.h>
+
+namespace sead {
+
+class AudioSoundHeapCafe;
+
+// TODO
+class AudioSoundDataMgrCafe // : public nw::snd::SoundDataManager
+{
+public:
+    void setContentRootPath(const char* content_root_path);
+    void mountSoundArchiveFromFs(const SafeString& path, Heap* heap, bool file_access_in_function, bool load_label_string);
+
+    void loadData(u32 id, u32 load_flag, u32 load_block_size, AudioSoundHeapCafe* sound_heap);
+    void loadData(const char* item_name, u32 load_flag, u32 load_block_size, AudioSoundHeapCafe* sound_heap);
+};
+
+} // namespace sead
